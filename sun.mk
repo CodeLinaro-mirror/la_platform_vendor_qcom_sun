bcd938b70b648be415e5aab14fcf86506a46f4db
Roopesh Nataraja <quic_roopeshr@quicinc.com>
2024-06-11 13:27:07 -0700

    sun: Align all 64-bit userspace ELF binaries to 16 KB
    Align all 64-bit userspace ELF binaries to 16 KB by setting
    PRODUCT_MAX_PAGE_SIZE_SUPPORTED := 16384
    
    Change-Id: I71cb978d84c3d1130a0e0edf3d712bf650d26193

--- sun.mk
+++ sun.mk
@@ -38,8 +38,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Set GRF/Vendor freeze properties
 BOARD_SHIPPING_API_LEVEL := 202404
 
-#Align ELF segment of binaries to 64k
-PRODUCT_MAX_PAGE_SIZE_SUPPORTED := 65536
+#Align all 64-bit userspace ELF binaries to 16 KB
+PRODUCT_MAX_PAGE_SIZE_SUPPORTED := 16384
 
 #Enable AOSP to be page size agnostic
 PRODUCT_NO_BIONIC_PAGE_SIZE_MACRO := true
