785937168dcd6d80146a5592ebba3ac2ea7d82ac
Shashank Sivakumar <quic_shassiva@quicinc.com>
2025-02-23 22:25:35 -0800

    sun: Disable 16k elf alignment check temporarily
    Disable 16k elf alignment check temporarily by setting
    PRODUCT_CHECK_PREBUILT_MAX_PAGE_SIZE to false. This change
    needs to be reverted as soon as all modules are aligned to 16k.
    
    Change-Id: Ia54a7ea59ff1b717bfc3dde0235e25e0365ff0bb

--- sun.mk
+++ sun.mk
@@ -19,6 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.m
 PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.threads=true
 
+#Disable the 16K page size checks
+PRODUCT_CHECK_PREBUILT_MAX_PAGE_SIZE := false
+
+
 # Enable debugfs restrictions
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
